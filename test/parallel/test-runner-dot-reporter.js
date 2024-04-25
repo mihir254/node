27fb4c0eef69dec7cd3092ae74ff9eb17b7d3af1
Mihir Bhansali <mbmihirbhansali@gmail.com>
2024-04-25 15:44:51 -0400

    test_runner: dot runner prints failed tests

--- test/parallel/test-runner-dot-reporter.js
+++ test/parallel/test-runner-dot-reporter.js
@@ -7,6 +7,14 @@ test('should run successfully', () => {
     assert.strictEqual(2+2, 4, '2 and 2 should give 4');
 });
 
-test('should fail', () => {
-    assert.strictEqual(2+2, 5, '2 and 2 should give 4');
+test('should fail 1', () => {
+    assert.strictEqual(2+2, 5, '2 and 3 should give 5');
+});
+
+test('should run successfully too', () => {
+    assert.strictEqual(1+1, 2, '1 and 1 should give 2');
+});
+
+test('should fail 2', () => {
+    assert.strictEqual(1+1, 3, '1 and 2 should give 3');
 });
